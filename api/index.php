a2f6fc492c1e57077f16885a6d4916227b604116
Wasem8 <waseemalbuqai.com>
2026-04-20 16:33:51 +0300

    fix 1

--- api/index.php
+++ api/index.php
@@ -1,3 +1,16 @@
 <?php
-// توجيه الطلبات إلى ملف الاندكس الأصلي في لارفيل
+
+// DEBUG ONLY - احذفه بعد التشخيص
+if (isset($_GET['debug_server'])) {
+    header('Content-Type: application/json');
+    echo json_encode([
+        'REQUEST_URI' => $_SERVER['REQUEST_URI'] ?? 'NOT SET',
+        'PATH_INFO' => $_SERVER['PATH_INFO'] ?? 'NOT SET',
+        'HTTP_X_FORWARDED_URI' => $_SERVER['HTTP_X_FORWARDED_URI'] ?? 'NOT SET',
+        'SCRIPT_NAME' => $_SERVER['SCRIPT_NAME'] ?? 'NOT SET',
+        'all_headers' => getallheaders(),
+    ]);
+    exit;
+}
+
 require __DIR__ . '/../public/index.php';
