ea4c2e7b96286e401316773b24d1b420f0e399c0
Wasem8 <waseemalbuqai.com>
2026-04-20 11:11:29 +0300

    fix api route

--- Modules/Invitation/routes/web.php
+++ Modules/Invitation/routes/web.php
@@ -3,6 +3,3 @@
 use Illuminate\Support\Facades\Route;
 use Modules\Invitation\Http\Controllers\InvitationController;
 
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::resource('invitations', InvitationController::class)->names('invitation');
-});
