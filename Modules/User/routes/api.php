4fda81c0d985c1103fa6c6fd04da485e135851ca
Wasem8 <waseemalbuqai.com>
2026-05-15 19:13:35 +0300

    updateFcmToken

--- Modules/User/routes/api.php
+++ Modules/User/routes/api.php
@@ -15,6 +15,7 @@ Route::prefix('auth')->group(function () {
         Route::post('/logout', [AuthController::class, 'logout']);
         Route::get('me', [AuthController::class, 'me']);
         Route::post('refresh', [AuthController::class, 'refresh']);
+        Route::post('/update-fcm-token', [AuthController::class, 'updateFcmToken']);
     });
 });
 
