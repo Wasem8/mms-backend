8feac88d2c4f9a70b4287a22baf84dbf3d1f93ae
Owais Aboud <aboudowais1@gmai.com>
2026-07-18 09:42:13 +0300

    certificate

--- Modules/Volunteer/Services/VolunteerEvaluationService.php
+++ Modules/Volunteer/Services/VolunteerEvaluationService.php
@@ -113,21 +113,21 @@ class VolunteerEvaluationService
 
         $html = view('volunteer::certificate', $data)->render();
 
-        $tempDir = storage_path('app/mpdf_cache');
+        $tempDir = '/tmp/mpdf_cache_volunteer';
         if (!is_dir($tempDir)) {
-            mkdir($tempDir, 0755, true);
+            @mkdir($tempDir, 0777, true);
         }
 
-        $localRegular = storage_path('app/fonts/Cairo-Regular.ttf');
-        $localBold = storage_path('app/fonts/Cairo-Bold.ttf');
+        $localRegular = '/tmp/Cairo-Regular.ttf';
+        $localBold = '/tmp/Cairo-Bold.ttf';
 
         if (!file_exists($localRegular)) {
             $remoteRegular = 'https://koihzqfwzvnrcrrtpnyg.supabase.co/storage/v1/object/public/assets/Cairo-Regular.ttf';
-            file_put_contents($localRegular, file_get_contents($remoteRegular));
+            @file_put_contents($localRegular, @file_get_contents($remoteRegular));
         }
         if (!file_exists($localBold)) {
             $remoteBold = 'https://koihzqfwzvnrcrrtpnyg.supabase.co/storage/v1/object/public/assets/Cairo-Bold.ttf';
-            file_put_contents($localBold, file_get_contents($remoteBold));
+            @file_put_contents($localBold, @file_get_contents($remoteBold));
         }
 
         $mpdf = new \Mpdf\Mpdf([
@@ -138,7 +138,7 @@ class VolunteerEvaluationService
             'margin_top'    => 8,
             'margin_bottom' => 8,
             'tempDir'       => $tempDir,
-            'fontDir'       => [storage_path('app/fonts')],
+            'fontDir'       => ['/tmp'],
             'fontdata'      => [
                 'cairo' => [
                     'R'      => 'Cairo-Regular.ttf',
@@ -158,15 +158,15 @@ class VolunteerEvaluationService
     {
         $pdfContent = $this->renderCertificateMpdf($volunteerId, $opportunityId);
 
-        $pdfDir = storage_path('app/certificates');
+        $pdfDir = '/tmp/certificates';
         if (!is_dir($pdfDir)) {
-            mkdir($pdfDir, 0755, true);
+            @mkdir($pdfDir, 0777, true);
         }
 
         $fileName = "volunteer_{$volunteerId}_opportunity_{$opportunityId}.pdf";
         $filePath = $pdfDir . '/' . $fileName;
 
-        file_put_contents($filePath, $pdfContent);
+        @file_put_contents($filePath, $pdfContent);
 
         return $filePath;
     }
