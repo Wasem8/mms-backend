ad79bc1465116ee7e4c0839c64af21ee6587d56c
Wasem8 <waseemalbuqai.com>
2026-08-09 09:47:54 +0300

    add active middelware

--- Modules/Dashboard/routes/api.php
+++ Modules/Dashboard/routes/api.php
@@ -7,34 +7,37 @@ use Modules\Dashboard\Http\Controllers\ParentDashboardController;
 use Modules\Dashboard\Http\Controllers\SupervisorDashboardController;
 use Modules\Dashboard\Http\Controllers\TeacherDashboardController;
 
-
 Route::prefix('dashboard')->group(function () {
-    Route::middleware(['auth:api', 'role:halaqa_supervisor'])->group(function () {
+
+    Route::middleware(['auth:api', 'active.user', 'role:halaqa_supervisor'])->group(function () {
         Route::get('/supervisor/export-pdf', [SupervisorDashboardController::class, 'exportPdf']);
         Route::get('supervisor/stats', [SupervisorDashboardController::class, 'formatted']);
     });
 
-    Route::middleware(['auth:api', 'role:halaqa_supervisor,mosque_manager'])->group(function () {
+    Route::middleware(['auth:api', 'active.user', 'role:halaqa_supervisor,mosque_manager'])->group(function () {
         Route::get('/supervisor/export-pdf', [SupervisorDashboardController::class, 'exportPdf']);
     });
-    Route::middleware(['auth:api', 'role:teacher'])->group(function () {
-    Route::get('/teacher/dashboard', [TeacherDashboardController::class, 'index']);
-            Route::get('/teacher/export-pdf', [TeacherDashboardController::class, 'exportPdf']);
+
+    Route::middleware(['auth:api', 'active.user', 'role:teacher'])->group(function () {
+        Route::get('/teacher/dashboard', [TeacherDashboardController::class, 'index']);
+        Route::get('/teacher/export-pdf', [TeacherDashboardController::class, 'exportPdf']);
         Route::get(
             '/teacher/bootstrap',
             [TeacherDashboardController::class, 'bootstrap']
         );
     });
 
-    Route::middleware(['auth:api', 'role:parent'])->group(function () {
+    Route::middleware(['auth:api', 'active.user', 'role:parent'])->group(function () {
         Route::get('/parent/dashboard', [ParentDashboardController::class, 'index']);
         Route::get('/parent/export-pdf', [ParentDashboardController::class, 'exportPdf']);
     });
 });
 
+Route::prefix('dashboard/mosque-manager')
+    ->middleware(['auth:api', 'active.user'])
+    ->group(function () {
 
-Route::prefix('dashboard/mosque-manager')->middleware(['auth:api'])->group(function () {
-    // 1. Endpoint شامل يعيد كافة بيانات اللوحة في Request واحد (الأسرع للـ Frontend)
-    Route::get('/', [MosqueManagerDashboardController::class, 'index']);
-
-});
+        // 1. Endpoint شامل يعيد كافة بيانات اللوحة في Request واحد
+        // (الأسرع للـ Frontend)
+        Route::get('/', [MosqueManagerDashboardController::class, 'index']);
+    });
